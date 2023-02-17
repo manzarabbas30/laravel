774c82f204badab37cbe96fb066108fa1c13a455
Manzar <manzarabbasshah1@gmail.com>
2023-02-17 23:31:34 +0500

    first blog

--- firstblog/routes/web.php
+++ firstblog/routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
+Route::view("home","/home");
 Route::view("about","/about");
-Route::view("about","/contact");
+Route::view("contact","/contact");
